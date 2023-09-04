2fc4dbebbe2ebb2a11412891d3246911b75ed235
Dylan Gage <dylangage2014@gmail.com>
2023-09-04 18:02:52 -0500

    Finished all the pages

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Personal-Website
+# This is my portfolio website i made for my web development class.
--- Script.js
+++ Script.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+// This console log is for the workshop requirement.
+
+console.log("Hello World");
+
+// These Varables below are for the "if Statements" further down.
+// They are meant to correspond to each page you can visit.
+
+const index = `Welcome to my website. You can learn about me and what i've made here on the "About" and "Portfolio" Page by clicking the links at the top.`;
+const about = "Welcome to my About page, you can learn about me here.";
+const portfolio =
+  "Welcome to my portfolio, you can see what i've created here.";
+
+// These "if Statements" will check to see what is in the url.
+// Then they will output a console log with ONE of the three appropriately named variables above according to whats in the URL.
+
+if (document.URL.includes("Index.html")) {
+  console.log(index);
+}
+
+if (document.URL.includes("About.html")) {
+  console.log(about);
+}
+
+if (document.URL.includes("Portfolio.html")) {
+  console.log(portfolio);
+}
